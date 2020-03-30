d9dce540dd632ebb753d5f6ad52c815f2f825157
Rachel Taverna <rtaverna96@gmail.com>
2020-03-30 11:23:34 -0400

    env db url

--- server/db/db.js
+++ server/db/db.js
@@ -2,10 +2,14 @@ const Sequelize = require("sequelize");
 const pkg = require("../../package.json");
 
 // const databaseName = pkg.name + (process.env.NODE_ENV === 'test' ? '-test' : '')
-
-const db = new Sequelize("postgres://localhost:5432/Stackathon", {
-  logging: false
-});
+let db
+if (process.env.DATABASE_URL) {
+  db = new Sequelize(process.env.DATABASE_URL, {
+    logging: false
+  });
+} else  {
+  db = new Sequelize("postgres://localhost:5432/Stackathon")
+}
 module.exports = db;
 
 // This is a global Mocha hook used for resource cleanup.
